297e09e15030687832163231eee03269cd3abad5
Rahul Shivsharan <rahulshivsharan@gmail.com>
2014-02-18 18:17:33 +0530

    Create CustomerDao.java

---
+++ java/com/dao/CustomerDao.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dao;
+
+import com.entities.Customer;
+import java.util.List;
+
+
+public interface CustomerDao {
+    
+    List<Customer> getCustomers() throws Exception;
+    
+    void saveCustomer(Customer customer) throws Exception;
+    
+    Customer getCustomer(Integer customerId) throws Exception;
+}
